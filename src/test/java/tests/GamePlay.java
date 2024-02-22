43588a57a73feafd4a73a98e52903b1104494162
nikola <djordjevic.nikola.it@gmail.com>
2024-02-22 20:22:49 +0100

    Cleaned up the code

--- src/test/java/tests/GamePlay.java
+++ src/test/java/tests/GamePlay.java
@@ -1,6 +1,5 @@
 package tests;
 
-import io.qameta.allure.Description;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Parameters;
